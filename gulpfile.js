57108e055d367769de542b14a042c7cf71aa4299
zeckson <evgenii.schepotiev@gmail.com>
2014-04-24 17:57:51 +0200

    build

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,32 @@
  */
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-    // place code for your default task here
+var rjs = require('gulp-requirejs');
+var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+
+var paths = {
+    main: 'app/scripts',
+    html: 'app/index.html',
+    outCss: 'bundle.css',
+    outJsFileName: 'bundle.js'
+};
+
+gulp.task('scripts', function () {
+    return rjs({name: 'main',
+        baseUrl: paths.main,
+        mainConfigFile: "app/scripts/main.js",
+        out: paths.outJsFileName}).
+        pipe(uglify()).
+        pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('html', function () {
+    return gulp.src(paths.html).
+        pipe(htmlreplace({
+            'js': paths.outJsFileName
+        })).
+        pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['scripts', 'html']);
